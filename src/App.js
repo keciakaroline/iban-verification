7f25149549f560810ea7bbc1f4d5ccdf57fdf4ba
Kécia Santos <keciakaroline@gmail.com>
2022-07-05 14:55:34 +0200

    Added tailwind & form skelec & styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto h-fit rounded max-w-screen-md m-40 p-28 bg-gradient-to-br from-cyan-50 to-sky-200 shadow-lg shadow-gray-400">
+      <div className="text-center mb-14 text-2xl font-semibold tracking-wide underline">
+        <h1>IBAN Verification</h1>
+      </div>
+      <div className="text-center text-lg flex flex-col">
+        <input
+          type="text"
+          placeholder="IBAN"
+          className="w-full mb-12 outline-none border-b-2 border-b-black bg-transparent"
+        />
+        <div>
+          <button
+            type="submit"
+            className="px-20 py-2 rounded-full border shadow-md shadow-gray-400 border-black hover:bg-sky-900 hover:text-white"
+          >
+            Check
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
