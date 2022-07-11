7645bd2701631f10c3caa6f9a408d2a49a92ca2e
Kécia Santos <keciakaroline@gmail.com>
2022-07-11 15:36:16 +0200

    Changed functions/states names & added css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import { checkIbanNumbers } from "./components/Validation";
+import "./App.css";
 
 function App() {
-  const [iban, setIban] = useState("");
-  const LI_IBAN = 21;
+  const [inputIban, setInputIban] = useState("");
 
   const handlerChangeIban = (e) => {
-    setIban(e.target.value);
+    setInputIban(e.target.value);
   };
-  //console.log(iban);
 
-  console.log(letterValue("LI"));
-
-  console.log(reOrderIban("1234567890"));
+  const handleVerification = () => {
+    checkIbanNumbers(inputIban);
+  };
 
   return (
-    <div className="mx-auto h-fit rounded max-w-screen-md m-40 p-28 bg-gradient-to-br from-cyan-50 to-sky-200 shadow-lg shadow-gray-400">
+    <div className="container mx-auto h-fit rounded max-w-screen-sm m-40 p-28 bg-gradient-to-br from-cyan-50 to-sky-200 shadow-lg shadow-gray-400">
       <div className="text-center mb-14 text-2xl font-semibold tracking-wide underline">
         <h1>IBAN Verification</h1>
       </div>
@@ -24,11 +23,13 @@ function App() {
           type="text"
           placeholder="IBAN"
           className="w-full mb-12 outline-none border-b-2 border-b-black bg-transparent"
-          value={iban}
+          value={inputIban}
           onChange={handlerChangeIban}
+          id="iban"
         />
         <div>
           <button
+            onClick={handleVerification}
             type="submit"
             className="px-20 py-2 rounded-full border shadow-md shadow-gray-400 border-black hover:bg-sky-900 hover:text-white"
           >
